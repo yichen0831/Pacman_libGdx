1f31225e9bac1008127aa0a6fb22dcf83ca51edf
Yichen Huang (Eugene) <yichen0831@gmail.com>
2015-11-17 20:37:05 +0800

    Improve player movement

--- core/src/com/ychstudio/systems/PlayerSystem.java
+++ core/src/com/ychstudio/systems/PlayerSystem.java
@@ -8,9 +8,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.RayCastCallback;
+import com.badlogic.gdx.physics.box2d.World;
 import com.ychstudio.components.MovementComponent;
 import com.ychstudio.components.PlayerComponent;
 import com.ychstudio.components.StateComponent;
+import com.ychstudio.gamesys.GameManager;
 
 public class PlayerSystem extends IteratingSystem {
 
@@ -18,7 +22,16 @@ public class PlayerSystem extends IteratingSystem {
     private ComponentMapper<MovementComponent> movementM = ComponentMapper.getFor(MovementComponent.class);
     private ComponentMapper<StateComponent> stateM = ComponentMapper.getFor(StateComponent.class);
 
-    private Vector2 tmpV = new Vector2();
+    private Vector2 tmpV1 = new Vector2();
+    private Vector2 tmpV2 = new Vector2();
+    private boolean canMove;
+
+    private enum MoveDir {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
 
     public PlayerSystem() {
         super(Family.all(PlayerComponent.class, MovementComponent.class, StateComponent.class).get());
@@ -26,34 +39,30 @@ public class PlayerSystem extends IteratingSystem {
 
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
-        PlayerComponent player = playerM.get(entity);
         StateComponent state = stateM.get(entity);
         MovementComponent movement = movementM.get(entity);
         Body body = movement.body;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            body.applyLinearImpulse(tmpV.set(movement.speed, 0).scl(body.getMass()), body.getWorldCenter(), true);
+        if (Gdx.input.isKeyPressed(Input.Keys.D) && checkMovable(body, MoveDir.RIGHT)) {
+            body.applyLinearImpulse(tmpV1.set(movement.speed, 0).scl(body.getMass()), body.getWorldCenter(), true);
+            state.setState(PlayerComponent.MOVE_RIGHT);
 
-        } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            body.applyLinearImpulse(tmpV.set(-movement.speed, 0).scl(body.getMass()), body.getWorldCenter(), true);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A) && checkMovable(body, MoveDir.LEFT)) {
+            body.applyLinearImpulse(tmpV1.set(-movement.speed, 0).scl(body.getMass()), body.getWorldCenter(), true);
+            state.setState(PlayerComponent.MOVE_LEFT);
 
-        } else if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            body.applyLinearImpulse(tmpV.set(0, movement.speed).scl(body.getMass()), body.getWorldCenter(), true);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.W) && checkMovable(body, MoveDir.UP)) {
+            body.applyLinearImpulse(tmpV1.set(0, movement.speed).scl(body.getMass()), body.getWorldCenter(), true);
+            state.setState(PlayerComponent.MOVE_UP);
 
-        } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            body.applyLinearImpulse(tmpV.set(0, -movement.speed).scl(body.getMass()), body.getWorldCenter(), true);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.S) && checkMovable(body, MoveDir.DOWN)) {
+            body.applyLinearImpulse(tmpV1.set(0, -movement.speed).scl(body.getMass()), body.getWorldCenter(), true);
+            state.setState(PlayerComponent.MOVE_DOWN);
 
         }
 
-        if (body.getLinearVelocity().x > 0.1f) {
-            state.setState(PlayerComponent.MOVE_RIGHT);
-        } else if (body.getLinearVelocity().x < -0.1f) {
-            state.setState(PlayerComponent.MOVE_LEFT);
-        } else if (body.getLinearVelocity().y > 0.1f) {
-            state.setState(PlayerComponent.MOVE_UP);
-        } else if (body.getLinearVelocity().y < -0.1f) {
-            state.setState(PlayerComponent.MOVE_DOWN);
-        } else {
+        if (body.getLinearVelocity().len2() < 0.1f) {
+
             switch (state.getState()) {
                 case PlayerComponent.MOVE_UP:
                 case PlayerComponent.IDLE_UP:
@@ -78,4 +87,43 @@ public class PlayerSystem extends IteratingSystem {
 
     }
 
+    private boolean checkMovable(Body body, MoveDir dir) {
+        canMove = true;
+        World world = body.getWorld();
+
+        RayCastCallback rayCastCallback = new RayCastCallback() {
+            @Override
+            public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
+                if (fixture.getFilterData().categoryBits == GameManager.WALL_BIT || fixture.getFilterData().categoryBits == GameManager.GATE_BIT) {
+                    canMove = false;
+                    return 0;
+                }
+                return 0;
+            }
+        };
+
+        for (int i = 0; i < 2; i++) {
+            tmpV1.set(body.getPosition());
+            switch (dir) {
+                case UP:
+                    tmpV2.set(body.getPosition().x - (i - 0.5f) * 0.2f, body.getPosition().y + 0.6f);
+                    break;
+                case DOWN:
+                    tmpV2.set(body.getPosition().x - (i - 0.5f) * 0.2f, body.getPosition().y - 0.6f);
+                    break;
+                case LEFT:
+                    tmpV2.set(body.getPosition().x - 0.6f, body.getPosition().y - (i - 0.5f) * 0.2f);
+                    break;
+                case RIGHT:
+                    tmpV2.set(body.getPosition().x + 0.6f, body.getPosition().y - (i - 0.5f) * 0.2f);
+                    break;
+                default:
+                    break;
+            }
+
+            world.rayCast(rayCastCallback, tmpV1, tmpV2);
+        }
+        
+        return canMove;
+    }
 }
