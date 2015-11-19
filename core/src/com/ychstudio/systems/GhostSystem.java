62b29ba0b08db1a399ba55778d54e4e98f6fbd51
Yichen Huang <yichen0831@gmail.com>
2015-11-19 21:08:20 +0800

    Add ghost animation

--- core/src/com/ychstudio/systems/GhostSystem.java
+++ core/src/com/ychstudio/systems/GhostSystem.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.ychstudio.components.GhostComponent;
 import com.ychstudio.components.MovementComponent;
 import com.ychstudio.components.StateComponent;
@@ -14,7 +15,7 @@ public class GhostSystem extends IteratingSystem {
     private final ComponentMapper<GhostComponent> ghostM = ComponentMapper.getFor(GhostComponent.class);
     private final ComponentMapper<MovementComponent> movementM = ComponentMapper.getFor(MovementComponent.class);
     private final ComponentMapper<StateComponent> stateM = ComponentMapper.getFor(StateComponent.class);
-    
+
     public GhostSystem() {
         super(Family.all(GhostComponent.class, MovementComponent.class, StateComponent.class).get());
     }
@@ -22,15 +23,32 @@ public class GhostSystem extends IteratingSystem {
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
         GhostComponent ghost = ghostM.get(entity);
-        
+        StateComponent state = stateM.get(entity);
+        MovementComponent movement = movementM.get(entity);
+        Body body = movement.body;
+
         if (ghost.getPosition().dst2(GameManager.instance.playerLocation.getPosition()) < 25f) {
             ghost.setBehavior(GhostComponent.ARRIVE_BEHAVIOR);
-        }
-        else {
+        } else {
             ghost.setBehavior(GhostComponent.WANDER_BEHAVIOR);
         }
-        
+
         ghost.update(deltaTime);
+
+        // for updating animation
+        if (body.getLinearVelocity().x > 0.1f) {
+            state.setState(GhostComponent.MOVE_RIGHT);
+
+        } else if (body.getLinearVelocity().x < -0.1f) {
+            state.setState(GhostComponent.MOVE_LEFT);
+
+        } else if (body.getLinearVelocity().y > 0.1f) {
+            state.setState(GhostComponent.MOVE_UP);
+
+        } else if (body.getLinearVelocity().y < -0.1f) {
+            state.setState(GhostComponent.MOVE_DOWN);
+
+        }
     }
-    
+
 }
